1b96e7a7f187284b673a7fc76953494c0440f74e
Alex <alexrex11@gmail.com>
2017-01-13 10:40:37 +0100

    Startpunkt der Navi ist jetzt der eigene Standort

--- app/src/main/java/gruppe2/demo/win/Fragments/navi_hhn.java
+++ app/src/main/java/gruppe2/demo/win/Fragments/navi_hhn.java
@@ -5,13 +5,19 @@ import android.content.Context;
 import android.app.Fragment;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Location;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -19,6 +25,10 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
 import com.akexorcist.googledirection.DirectionCallback;
 import com.akexorcist.googledirection.GoogleDirection;
@@ -31,9 +41,23 @@ import java.util.ArrayList;
 import gruppe2.demo.win.R;
 
 
-public class navi_hhn extends Fragment implements OnMapReadyCallback, DirectionCallback {
+public class navi_hhn extends Fragment implements OnMapReadyCallback, DirectionCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
+    //Variablen
     private GoogleMap mMap;
+    //Positonsbestimmung
+    //Mit Hilfe dieser können wir später die Position auslesen
+    GoogleApiClient mGoogleApiClient;
+    //Diese Variable beinhaltet später die Positionsdaten
+    Location mLastLocation;
+
+    double laengengrad;
+    double breitengrad;
+
+    //Navigation
+    String serverKey = "AIzaSyDytcF7j1kQPtIKDhRYPjcssNwoyp7yVzE";
+    LatLng destination = new LatLng(49.122635, 9.206136);
+
 
     @Nullable
     @Override
@@ -47,7 +71,9 @@ public class navi_hhn extends Fragment implements OnMapReadyCallback, DirectionC
 
         MapFragment fragment = (MapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         fragment.getMapAsync(this);
+
     }
+
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -59,14 +85,61 @@ public class navi_hhn extends Fragment implements OnMapReadyCallback, DirectionC
         //googleMap.addMarker(new MarkerOptions().title("HS Heilbronn").position(SONTHEIM));
 
         mMap.setMyLocationEnabled(true);
-        requestDirection();
+        standortbestimmung();
+    }
+
+
+
+    public void standortbestimmung() {
+        //Aufruf der Initalisierung
+        buildGoogleApiClient();
+
+        //Starten der eben initalisierten API von Google
+        mGoogleApiClient.connect();
 
     }
-    String serverKey = "AIzaSyDytcF7j1kQPtIKDhRYPjcssNwoyp7yVzE";
-    LatLng origin = new LatLng(48.9190624, 9.3068855);
-    LatLng destination = new LatLng(49.282437, 9.3281893);
+
+    //Initalisiert die API von Google, mit der wir die Position auslesen können
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(getContext())
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+
+    //Wird aufgerufen, wenn die Verbindung zur Google API erfolgreich ist
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        //Anhand der erfolgreichen Verbindung die benötigten Daten auslesen
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+        String stringlaengengrad = String.valueOf(mLastLocation.getLongitude());
+        String stringbreitengrad = String.valueOf(mLastLocation.getLatitude());
+        laengengrad = Double.parseDouble(stringlaengengrad);
+        breitengrad = Double.parseDouble(stringbreitengrad);
+
+        requestDirection();
+    }
+
+
+
+    //Wird aufgerufen, wenn die Verbindung zur Google API aussteht
+    @Override
+    public void onConnectionSuspended(int i) {}
+
+    //Wird aufgerufen, wenn die Verbindung zur Google API fehlschlägt
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        //l.setText("Längengrad: Da ist ein Fehler aufgetreten.");
+        //b.setText("Breitengrad: Da ist ein Fehler aufgetreten.");
+    }
+
+
 
     public void requestDirection() {
+        LatLng origin = new LatLng(breitengrad, laengengrad);
         GoogleDirection.withServerKey(serverKey)
                 .from(origin)
                 .to(destination)
@@ -78,6 +151,7 @@ public class navi_hhn extends Fragment implements OnMapReadyCallback, DirectionC
     @Override
     public void onDirectionSuccess(Direction direction, String rawBody) {
     if (direction.isOK()) {
+        LatLng origin = new LatLng(breitengrad, laengengrad);
         mMap.addMarker(new MarkerOptions().position(origin));
         mMap.addMarker(new MarkerOptions().position(destination));
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(origin, 11));
@@ -89,6 +163,6 @@ public class navi_hhn extends Fragment implements OnMapReadyCallback, DirectionC
 
     @Override
     public void onDirectionFailure(Throwable t) {
-
+        System.out.println("Fehler bei der Routenberechnung");
     }
 }
