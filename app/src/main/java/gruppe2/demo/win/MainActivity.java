8654beea928a4468e1fe4df2cbd526321f994f02
agruenth <adrian.gruenther@gmail.com>
2017-01-13 15:43:23 +0100

    spinner mit problem

--- app/src/main/java/gruppe2/demo/win/MainActivity.java
+++ app/src/main/java/gruppe2/demo/win/MainActivity.java
@@ -1,9 +1,12 @@
 package gruppe2.demo.win;
 
+import android.Manifest;
 import android.app.FragmentManager;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,7 +15,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -22,6 +30,8 @@ import gruppe2.demo.win.Fragments.navi_hhn;
 import gruppe2.demo.win.Fragments.infos_hhn;
 
 public class MainActivity extends AppCompatActivity
+
+
         implements NavigationView.OnNavigationItemSelectedListener,
         GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener {
@@ -36,7 +46,10 @@ public class MainActivity extends AppCompatActivity
     TextView l;
     TextView b;
 
+    // Variablen für Spinner
 
+    Spinner spinner;
+    ArrayAdapter<CharSequence> adapter;
 
 
 
@@ -70,6 +83,28 @@ public class MainActivity extends AppCompatActivity
         FragmentManager fm = getFragmentManager();
         fm.beginTransaction().replace(R.id.content_frame, new navi_hhn()).commit();
 
+
+        setContentView(R.layout.activity_main);
+
+
+        spinner = (Spinner)findViewById(R.id.spinner);
+        adapter = ArrayAdapter.createFromResource(this,R.array.spinner,android.R.layout.simple_spinner_item );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                Toast.makeText(getBaseContext(), parent.getItemAtPosition(position) + " selected", Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
+
     }
 
     @Override
@@ -102,6 +137,16 @@ public class MainActivity extends AppCompatActivity
     @Override
     public void onConnected(Bundle connectionHint) {
         //Anhand der erfolgreichen Verbindung die benötigten Daten auslesen
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
         mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
 
         if (mLastLocation != null) {
@@ -111,34 +156,41 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    //Wird aufgerufen, wenn die Verbindung zur Google API aussteht
-    @Override
-    public void onConnectionSuspended(int i) {}
-
-    //Wird aufgerufen, wenn die Verbindung zur Google API fehlschlägt
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-        l.setText("Längengrad: Da ist ein Fehler aufgetreten.");
-        b.setText("Breitengrad: Da ist ein Fehler aufgetreten.");
-    }
 
 
 
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        FragmentManager fm = getFragmentManager();
 
-        int id = item.getItemId();
 
-        if (id == R.id.infos_hhn) {
-            fm.beginTransaction().replace(R.id.content_frame, new infos_hhn()).commit();
-        } else if (id == R.id.navi_hhn) {
-            fm.beginTransaction().replace(R.id.content_frame, new navi_hhn()).commit();
+
+
+                //Wird aufgerufen, wenn die Verbindung zur Google API aussteht
+        @Override
+        public void onConnectionSuspended ( int i){
         }
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
 
-}
+        //Wird aufgerufen, wenn die Verbindung zur Google API fehlschlägt
+        @Override
+        public void onConnectionFailed (ConnectionResult connectionResult){
+            l.setText("Längengrad: Da ist ein Fehler aufgetreten.");
+            b.setText("Breitengrad: Da ist ein Fehler aufgetreten.");
+        }
+
+
+        @SuppressWarnings("StatementWithEmptyBody")
+        @Override
+        public boolean onNavigationItemSelected (MenuItem item){
+            FragmentManager fm = getFragmentManager();
+
+            int id = item.getItemId();
+
+            if (id == R.id.infos_hhn) {
+                fm.beginTransaction().replace(R.id.content_frame, new infos_hhn()).commit();
+            } else if (id == R.id.navi_hhn) {
+                fm.beginTransaction().replace(R.id.content_frame, new navi_hhn()).commit();
+            }
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            drawer.closeDrawer(GravityCompat.START);
+            return true;
+        }
+
+    }
